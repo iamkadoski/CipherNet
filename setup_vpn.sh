7939d1cb8ac92ae6015c0b4c1d8ca7787d5d0cd3
Monday Kadiri <100347326+iamkadoski@users.noreply.github.com>
2024-08-28 12:30:37 +0100

    Update setup_vpn.sh

--- setup_vpn.sh
+++ setup_vpn.sh
@@ -17,7 +17,6 @@ MODE=$2
 AES_KEY=$3
 
 if [ "$MODE" == "SERVER" ]; then
-  # Start the VPN server
   ./$APP_NAME SERVER $AES_KEY &
   SERVER_PID=$!
   echo "VPN server started with PID: $SERVER_PID"
@@ -28,11 +27,12 @@ elif [ "$MODE" == "CLIENT" ]; then
     exit 1
   fi
   SERVER_IP=$4
-  # Start the VPN client
   ./$APP_NAME CLIENT $AES_KEY $SERVER_IP &
   CLIENT_PID=$!
   echo "VPN client started with PID: $CLIENT_PID"
 
+  # Check if the route exists before adding it
+  ip route show | grep -q "^10.8.0.0/16" || ip route add 10.8.0.0/16 via $SERVER_IP
 else
   echo "Invalid mode. Use 'SERVER' or 'CLIENT'."
   exit 1
